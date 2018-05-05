13c97922f30f9ada4fb17b209d922cbaea5890a0
kazancevs <s@kazancev.xyz>
2018-05-05 15:31:04 +0300

    Excel file initialise has been added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import xlsxwriter
 import sorts
+import generate
 
-kek = [2, 5, 3, 1]
-print(sorts.shell(kek))
+data_types = ['byte', 'int', 'string', 'date']
+data_volumes = [50, 500, 5000, 50000, 500000]
+
+workbook = xlsxwriter.Workbook('bubble.xlsx')
+worksheet = workbook.add_worksheet('straight')
+for i in range(len(data_volumes)):
+    worksheet.write(0, i + 1, data_volumes[i])
+for i in range(len(data_types)):
+    worksheet.write(i + 1, 0, data_types[i])
+
